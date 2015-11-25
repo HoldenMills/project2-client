510267e425d42e032c16990e59c251d0f7aa13e6
Holden Mills <mholdenmills@gmail.com>
2015-11-25 16:30:20 -0500

    Initiated javascript.js, ajax.js and user_session.js
    Created register, login and config functions

---
+++ api/ajax.js
@@ -0,0 +1,33 @@
+var tttapi = {
+ gameWatcher: null,
+ ttt: 'http://ttt.wdibos.com',
+
+ ajax: function(config, cb) {
+   $.ajax(config).done(function(data, textStatus, jqxhr) {
+     cb(null, data);
+   }).fail(function(jqxhr, status, error) {
+     cb({jqxher: jqxhr, status: status, error: error});
+   });
+ },
+
+ register: function register(credentials, callback) {
+   this.ajax({
+     method: 'POST',
+     // url: 'http://httpbin.org/post',
+     url: this.ttt + '/users',
+     contentType: 'application/json; charset=utf-8',
+     data: JSON.stringify(credentials),
+     dataType: 'json'
+   }, callback);
+ },
+
+ login: function login(credentials, callback) {
+   this.ajax({
+     method: 'POST',
+     // url: 'http://httpbin.org/post',
+     url: this.ttt + '/login',
+     contentType: 'application/json; charset=utf-8',
+     data: JSON.stringify(credentials),
+     dataType: 'json'
+   }, callback);
+ },
