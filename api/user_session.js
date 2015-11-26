a4e2c81c06b1d5a0361c2455e6b80be529aa34dd
Holden Mills <mholdenmills@gmail.com>
2015-11-25 23:00:07 -0500

    Working on show function for park_list

--- api/user_session.js
+++ api/user_session.js
@@ -5,21 +5,21 @@ var url = 'http://www.localhost:3000';
 $(document).ready (function() {
 
   var form2object = function(form) {
-  var data = {};
-  $(form).children().each(function(index, element) {
-    var type = $(this).attr('type');
-    if ($(this).attr('name') && type !== 'submit' && type !== 'hidden') {
-      data[$(this).attr('name')] = $(this).val();
-    }
-  });
-  return data;
+    var data = {};
+    $(form).children().each(function(index, element) {
+      var type = $(this).attr('type');
+      if ($(this).attr('name') && type !== 'submit' && type !== 'hidden') {
+        data[$(this).attr('name')] = $(this).val();
+      }
+    });
   };
+  return data;
+  });
 
   var wrap = function wrap(root, formData) {
     var wrapper = {};
     wrapper[root] = formData;
     return wrapper;
-  };
 
   $('#register').on('submit', function(e) {
     var credentials = wrap('credentials', form2object(this));
@@ -38,5 +38,5 @@ $(document).ready (function() {
     url.login(credentials, cb);
     };
   });
-});
+};
 
